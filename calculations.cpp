9ec4de12d62bcdfb89afe2f4457b3edf934bd383
basiibas <bas.riemprasert@gmail.com>
2018-04-22 00:47:24 +0200

    Sletter tidligere versjon

--- calculations.cpp
+++
@@ -1,6 +0,0 @@
-#include <calculations.h>
-
-double profitCalc(double currency, double amount,int buyPrice){
-
-    return (buyPrice*amount)-(currency*amount);
-}
--- calculations.h
+++
@@ -1,8 +0,0 @@
-#ifndef CALCULATIONS_H
-#define CALCULATIONS_H
-
-//Functions
-double profitCalc(double currency, double amount,int buyPrice);
-
-
-#endif // CALCULATIONS_H
