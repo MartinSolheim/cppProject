bd80391794801ab3948d7f5b7901087b7c9eb499
Martin Solheim <martyzno@gmail.com>
2018-04-24 22:47:33 +0200

    ferdig2

--- ccpProjectFinal/calculations.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATIONS_H
-#define CALCULATIONS_H
-
-//Beregningsfunksjon
-//Beregner hvor mye man har tjent eller tapt siden man kjøpte kryptovaluta
-double profitCalc(double currency, double amount,int buyPrice);
-
-
-#endif // CALCULATIONS_H
