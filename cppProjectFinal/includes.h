0aa2914cf21674691987bdc1b4131d1533a3499e
Martin Solheim <martyzno@gmail.com>
2018-07-18 01:48:12 +0200

    small fixes

---
+++ cppProjectFinal/includes.h
@@ -0,0 +1,38 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+//QtGUI klasse- Widget
+#include <QTableWidget>
+#include <QLabel>
+#include <QDoubleSpinBox>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QComboBox>
+#include <QMenu>
+#include <QMenuBar>
+
+#include <QtCharts/QChartView>
+#include <QtCharts/QChart>
+#include <QtCharts/QStackedBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QLegend>
+
+//Qt klasse- Verktøy
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QtNetwork/QtNetwork>
+#include <iostream>
+#include <QVector>
+#include <time.h>
+#include <QDebug>
+#include <QChartView>
+#include <QStandardItem>
+#include <QtNetwork/QtNetwork>
+
+#endif // INCLUDES_H
---
+++ cppProjectFinal/widget.h
@@ -0,0 +1,101 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QtWidgets>
+#include <includes.h>
+
+QT_CHARTS_USE_NAMESPACE
+
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+    //Widget funksjoner
+    void fetchData();
+    void drawWidget();
+    void currencyGetData();
+    void table1GetData();
+    void showChart();
+    void timerFunc();
+    void updateProfit();
+
+
+private:
+    QJsonArray jsonArray;
+    QJsonArray ja;
+    QNetworkAccessManager networkManager;
+
+    //Menu bar
+    QMenuBar* menuBar;
+    QMenu*fileMenu;
+
+    //Qtgui klasser- Objekter
+
+    //BarChart layout///////
+    //Diagram(BarChart)
+    QChartView* chartView;
+    QChart* chart;
+    QStackedBarSeries* series;
+
+    //TableViews layout///////
+    //Filhåndterings knapper(QPushButton)
+    QTableView* table1;
+    QStandardItemModel *table1Model;
+
+    //Kjøpt table(Tableview)
+    QTableView* table2;
+    QStandardItemModel *table2Model;
+
+    //Profit(Input) Layout///////////
+    //Value input(Currency)
+    QPushButton* saveButton;
+    QPushButton* loadButton;
+    QPushButton* clearButton;
+
+    //Profit Layout///////////
+    //Currency
+    QLabel*currencyLabel;
+    QComboBox* currencyCBox;
+
+    //Mengde input(Amount)
+    QLabel* amountLabel;
+    QDoubleSpinBox* amountDSBox;
+
+    //Kjøpspris input(BuyPrice)
+    QLabel*buyPriceLabel;
+    QSpinBox* buyPriceSBox;
+
+    //Fortjeneste submit(Profit)
+    QPushButton* profitButton;
+
+    //Init button
+    QPushButton* initButton;
+
+    //Timer to fetch values every 5 min
+    QTimer timer;
+    bool timerIsRunning;
+
+    //Calculating variables
+    double amount;
+    int buyPrice;
+    double total;
+
+    QJsonObject object;
+
+//slots funksjoner som vi deklarerer for å utføre forskjellige operasjoner ved et knappetrykk
+//som å lagre og laste opp fil og oppdatere diagrammet
+private slots:
+    void profitOnClick();
+    void saveOnClick();
+    void loadOnClick();
+    void clearOnClick();
+    void initOnClick();
+};
+
+#endif // WIDGET_H
